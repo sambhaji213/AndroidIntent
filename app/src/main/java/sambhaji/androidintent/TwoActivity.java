a61d4491fdb703c85383479418020ec74367d598
Sambhaji <sambhaji2134@gmail.com>
2016-09-11 11:31:57 +0530

    An Android Intent is an abstract description of an operation to be performed.
    It can be used with startActivity to launch an Activity, broadcastIntent
    to send it to any interested BroadcastReceiver components, and
    startService(Intent) or bindService(Intent, ServiceConnection, int) to
    communicate with a background Service.

---
+++ app/src/main/java/sambhaji/androidintent/TwoActivity.java
@@ -0,0 +1,36 @@
+package sambhaji.androidintent;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.finish();
+    }
+}
